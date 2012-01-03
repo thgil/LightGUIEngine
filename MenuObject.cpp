5e0fb6ccda6128806390496242d135dad0ba0d81
Light <leenf@tcd.ie>
2012-01-03 14:24:57 +0100

    New class added

---
+++ MenuObject.cpp
@@ -0,0 +1,11 @@
+/*
+ *  MenuObject.cpp
+ *  LightGameTemplate
+ *
+ *  Created by Fergus Leen on 03/01/2012.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "MenuObject.h"
+
---
+++ MenuObject.h
@@ -0,0 +1,9 @@
+/*
+ *  MenuObject.h
+ *  LightGameTemplate
+ *
+ *  Created by Fergus Leen on 03/01/2012.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
