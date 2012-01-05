0edf437b0a7ea7c628d1e5c3a7bb083eddb5e2b5
Light <leenf@tcd.ie>
2012-01-05 03:40:18 +0000

    Made MenuObject into its own class instead of being a struct

--- MenuObject.h
+++ MenuObject.h
@@ -1,9 +1,24 @@
-/*
- *  MenuObject.h
- *  LightGameTemplate
- *
- *  Created by Fergus Leen on 03/01/2012.
- *  Copyright 2012 __MyCompanyName__. All rights reserved.
- *
- */
+#ifndef MENUOBJECT_H
+#define MENUOBJECT_H
 
+#include <list>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class MenuObject
+{
+public:
+	int action;		
+	
+	sf::Vector2f Pos; // Position
+	sf::Rect<int> *rect; // x1,y1,x2,y2 
+	sf::Sprite sprite;
+	sf::String text;
+	
+	enum State{Normal, Focused} _state;
+	void SetPosition(float x,float y){Pos.x=x;Pos.y=y;}
+	void CreateRect(float x,float y){rect = new sf::Rect<int>(Pos.x,Pos.y,Pos.x+x,Pos.y+y);}
+//	virtual void OnFocused();
+};
+
+#endif // MENUOBJECT_H
